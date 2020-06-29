02a493ca9683eb06b8bd41afb8c1151fc1d81a5d
hourq <houruiqiang1124@163.com>
2020-06-29 15:07:18 +0800

    增加svg

---
+++ src/layout/components/Navbar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="header_container">
+
+		<el-breadcrumb separator="/">
+			<el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+			<!-- <el-breadcrumb-item v-for="(item, index) in $route.meta.breadcrumb" :key="index">{{item}}</el-breadcrumb-item> -->
+		</el-breadcrumb>
+		<el-dropdown @command="handleCommand" menu-align='start'>
+			<div class="rightavatorBox">
+				<span>Hi, Admin</span>
+				<img src="@/assets/image/avatar.jpg" class="avator">
+			</div>
+			<el-dropdown-menu slot="dropdown">
+				<el-dropdown-item command="home">首页</el-dropdown-item>
+				<el-dropdown-item command="signout">退出</el-dropdown-item>
+			</el-dropdown-menu>
+		</el-dropdown>
+    </div>
+</template>
+
+<script>
+export default {
+	mounted() {
+		console.log(this.$route.meta);
+		
+	},
+    methods: {
+        handleCommand(command) {
+           if (command == 'home') {
+				this.$router.push('/home');
+            }else if(command == 'signout'){
+                this.$message({
+                    type: 'success',
+                    message: '退出成功'
+                });
+                this.$router.push('/');
+            }
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.header_container{
+		background-color: #EFF2F7;
+		height: 50px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding-left: 20px;
+	}
+	
+	.rightavatorBox {
+		display: flex;
+		align-items: center;
+		.rightName {
+
+		}
+		.avator{
+			width: 36px;
+			height: 36px;
+			border-radius: 50%;
+			margin-right: 20px;
+			margin-left: 20px;;
+		}
+	}
+</style>
---
+++ src/layout/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="app-wrapper">
+        <side-bar class="sidebar-container" />
+        <div class="main-container">
+            <div>
+                <nav-bar />
+            </div>
+            <app-main />
+        </div>
+    </div>
+</template>
+
+<script>
+import NavBar from './components/Navbar'
+import AppMain from './components/AppMain'
+import SideBar from './components/Sidebar/index'
+export default {
+components: {
+    NavBar,
+    AppMain,
+    SideBar
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+    .app-wrapper {
+        position: relative;
+        height: 100%;
+        width: 100%;
+        &.mobile.openSidebar{
+            position: fixed;
+            top: 0;
+        }
+    }
+
+</style>
