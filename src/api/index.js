c7693241656d0e6be0a3e50b76610d87d3b1be40
hourq <houruiqiang1124@163.com>
2020-07-20 13:39:41 +0800

    axios

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
-import request from '@/utils/request';
+import {post} from '@/utils/request';
 
 // 登录
 export const login = param => request("/index.php?app=login", param, 'post');
 
 // 会员经验值列表
-export const getMemberExperienceList = param => request("static/mock/memberExperience.json");
+export const getList = param => post("/website/nc/masterData/adminRegionList", param);
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,84 +1,68 @@
 import axios from 'axios'
-import { MessageBox, Message } from 'element-ui'
+import {
+	MessageBox,
+	Message
+} from 'element-ui'
 // import store from '@/store'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000 // request timeout
+	baseURL: "/api", // url = base url + request url
+	// withCredentials: true, // send cookies when cross-domain requests
+	timeout: 5000 // request timeout
 })
 
 // request interceptor
 service.interceptors.request.use(
-  config => {
-    // do something before request is sent
-
-    // if (store.getters.token) {
-    //   // let each request carry token
-    //   // ['X-Token'] is a custom headers key
-    //   // please modify it according to the actual situation
-    //   config.headers['X-Token'] = getToken()
-    // }
-    return config
-  },
-  error => {
-    // do something with request error
-    console.log(error) // for debug
-    return Promise.reject(error)
-  }
+	config => {
+		return config
+	},
+	error => {
+		return Promise.reject(error)
+	}
 )
 
-// response interceptor
-service.interceptors.response.use(
-  /**
-   * If you want to get http information such as headers or status
-   * Please return  response => response
-  */
+/*网络请求部分*/
 
-  /**
-   * Determine the request status by custom code
-   * Here is just an example
-   * You can also judge the status by HTTP Status Code
-   */
-  response => {
-    const res = response.data
+/*
+ *  get请求
+ *  url:请求地址
+ *  params:参数
+ * */
+export function get(url, params = {}) {
+	return new Promise((resolve, reject) => {
+		httpService({
+			url: url,
+			method: 'get',
+			params: params
+		}).then(response => {
+			resolve(response);
+		}).catch(error => {
+			reject(error);
+		});
+	});
+}
 
-    // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
-      Message({
-        message: res.message || 'Error',
-        type: 'error',
-        duration: 5 * 1000
-      })
+/*
+ *  post请求
+ *  url:请求地址
+ *  params:参数
+ * */
+export function post(url, params = {}) {
+	return new Promise((resolve, reject) => {
+		service({
+			url: url,
+			method: 'post',
+			data: params,
+			timeout: 5000
+		}).then(response => {
+			resolve(response);
+		}).catch(error => {
+			reject(error);
+		});
+	});
+}
 
-      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
-          type: 'warning'
-        }).then(() => {
-          // store.dispatch('user/resetToken').then(() => {
-          //   location.reload()
-          // })
-        })
-      }
-      return Promise.reject(new Error(res.message || 'Error'))
-    } else {
-      return res
-    }
-  },
-  error => {
-    console.log('err' + error) // for debug
-    Message({
-      message: error.message,
-      type: 'error',
-      duration: 5 * 1000
-    })
-    return Promise.reject(error)
-  }
-)
-
-export default service
+export default {
+	post
+}
